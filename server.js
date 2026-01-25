d34e080832bf5082049a1c2640d63a993c033085
Gurpreet <ajbagdi6@gmail.com>
2026-01-25 06:10:04 +0530

    updated

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: ["http://localhost:5173", "https://your-frontend-app.onrender.com"], // frontend URL
     credentials: true, // mandatory for cookies
   }),
 );
